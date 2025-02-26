797eec576c546dd44d33c408db40aa804cb38d72
Nur Rofiq <rofiq.nur.std.1560@gmail.com>
2025-02-26 21:17:18 +0700

    membuat folder

---
+++ backend/cmd/myapp/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New(fiber.Config{
+		IdleTimeout: time.Second * 5,
+		WriteTimeout: time.Second * 5,
+		ReadTimeout: time.Second * 5,
+	})
+
+	// Routing
+	app.Get("/", func(ctx *fiber.Ctx) error {
+		return ctx.SendString("Hello World")
+	})
+
+	err := app.Listen("localhost:3000")
+	if err != nil {
+		panic(err)
+	}
+
+	// Paused at 41:00
+}
